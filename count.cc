579f46694d607ea3ba2fdce163d1a904d4002115
choi_lab2 <wlsdjddl@gmail.com>
2022-09-05 19:30:54 +0900

    lab2

---
+++ count.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "count.h"
+
+using namespace std;
+void count_(int num)
+{
+	for(int i=1;i<=num;i++){
+		cout << i << endl;
+	}
+	cout << "Start!!"<<endl;
+	cout << "2017313219 jinyounchoi"<<endl;
+}
---
+++ main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "count.h"
+using namespace std;
+
+int main(){
+	int num;
+	cin >> num;
+	cout << "Hello, world!" <<endl;
+	count_(num);
+	return 0;
+}
